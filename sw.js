39b8e88270cf88053431e66a2b226a7e677b6eed
igamboa77 <68474131+igamboa77@users.noreply.github.com>
2023-11-01 22:18:36 -0700

    primer comit

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = `temperature-converter-v1`;
+    
+// Utilice el evento de instalación para almacenar en caché previamente todos los recursos iniciales.
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    cache.addAll([
+      './',
+      './converter.js',
+      './converter.css'
+    ]);
+  })());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+
+    // Obtenga el recurso del caché.
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    } else {
+        try {
+          // If the resource was not in the cache, try the network.
+          const fetchResponse = await fetch(event.request);
+    
+          // Save the resource in the cache and return it.
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        } catch (e) {
+          // The network failed
+        }
+    }
+  })());
+});
